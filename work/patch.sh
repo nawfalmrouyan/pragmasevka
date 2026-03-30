fe068f25068d5f805e1d1040875cda5c29408bc0
OpalBSD <nawfalmrouyan@gmail.com>
2026-03-30 22:39:16 +0800

    add config file to build the font and patch script used to create NF and NF Propop versions of the font

---
+++ work/patch.sh
@@ -0,0 +1,64 @@
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedbolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedheavyitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedheavyupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedregularitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedregularupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedsemibolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-condensedsemiboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalbolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalheavyitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalheavyupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalregularitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalregularupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalsemibolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevkaserif-normalsemiboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedbolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedheavyitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedheavyupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedregularitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedregularupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedsemibolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-condensedsemiboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalbolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalboldupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalheavyitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalheavyupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalregularitalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalregularupright.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalsemibolditalic.ttf
+../font-patcher -c --variable-width-glyphs --progressbars pragmasevka-normalsemiboldupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedbolditalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedboldupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedheavyitalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedheavyupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedregularitalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedregularupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedsemibolditalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-condensedsemiboldupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalbolditalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalboldupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalheavyitalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalheavyupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalregularitalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalregularupright.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalsemibolditalic.ttf
+../font-patcher -c --progressbars pragmasevkaserif-normalsemiboldupright.ttf
+../font-patcher -c --progressbars pragmasevka-condensedbolditalic.ttf
+../font-patcher -c --progressbars pragmasevka-condensedboldupright.ttf
+../font-patcher -c --progressbars pragmasevka-condensedheavyitalic.ttf
+../font-patcher -c --progressbars pragmasevka-condensedheavyupright.ttf
+../font-patcher -c --progressbars pragmasevka-condensedregularitalic.ttf
+../font-patcher -c --progressbars pragmasevka-condensedregularupright.ttf
+../font-patcher -c --progressbars pragmasevka-condensedsemibolditalic.ttf
+../font-patcher -c --progressbars pragmasevka-condensedsemiboldupright.ttf
+../font-patcher -c --progressbars pragmasevka-normalbolditalic.ttf
+../font-patcher -c --progressbars pragmasevka-normalboldupright.ttf
+../font-patcher -c --progressbars pragmasevka-normalheavyitalic.ttf
+../font-patcher -c --progressbars pragmasevka-normalheavyupright.ttf
+../font-patcher -c --progressbars pragmasevka-normalregularitalic.ttf
+../font-patcher -c --progressbars pragmasevka-normalregularupright.ttf
+../font-patcher -c --progressbars pragmasevka-normalsemibolditalic.ttf
+../font-patcher -c --progressbars pragmasevka-normalsemiboldupright.ttf
